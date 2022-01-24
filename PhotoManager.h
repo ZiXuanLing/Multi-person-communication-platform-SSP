b3ebcce3601d51c7a7a096310a48cff80703e71c
ZiXuanLing <2226958871@qq.com>
2022-01-24 15:07:50 +0800

    play a while

--- PhotoManager.h
+++ PhotoManager.h
@@ -4,8 +4,21 @@
 
 #include "PhotoInfo.h"
 
+#include "common/ret_value.h"
+
 class PhotoManager {
-    
+private:
+    PhotoInfo photos_[10240];
+    GETSETVAR(int, photo_count)
+public:
+    void Start();
+    void Proc();
+    void Shutdown();
+    void Restart();
+public:
+    PhotoInfo* GetPhoto(int user_id);
+    int UpdatePhoto(int user_id, int publisher_id, int publish_time, int publish_message_id);
+
 };
 
 #endif // !__PHOTO_MANAGER_H__
--- main.cc
+++ main.cc
@@ -1,17 +1,43 @@
 #include "UserManager.h"
+#include "RelationManager.h"
+#include "MessageManager.h"
+#include "PhotoManager.h"
 
 #include <stdio.h>
 #include <unistd.h>
 
 UserManager user_svr;
+RelationManager relation_svr;
+MessageManager message_svr;
+PhotoManager photo_svr;
 
 int main() {
     user_svr.Start();
-
+    relation_svr.Start();
+    message_svr.Start();
+    photo_svr.Start();
     while (1) {
-        user_svr.Proc();
-        sleep(1);
+        int type = 0; //
+        switch (type) {
+            case 1:
+                user_svr.Proc();
+                break;
+            case 2:
+                relation_svr.Proc();
+                break;
+            case 3:
+                message_svr.Proc();
+                break;
+            case 4:
+                photo_svr.Proc();
+                break;
+        }
+        usleep(5000);
+        break;
     }
     user_svr.Shutdown();
+    relation_svr.Shutdown();
+    message_svr.Shutdown();
+    photo_svr.Shutdown();
     return 0;
 }
